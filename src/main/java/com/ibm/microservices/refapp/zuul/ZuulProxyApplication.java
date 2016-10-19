0682b5c384d153b15f0034bbe8b57162106121e5
jkwong888 <jkwong888@users.noreply.github.com>
2016-10-18 20:46:34 -0400

    zuul JWT authentication filter (#2)
    * JWT validation filter
    
    * return UNAUTHORIZED for all failed authentication validations
    
    * add some debug
    
    * change "Authentication" to "Authorization"
    
    * debug -- parse token before fail verification
    
    * Base64 the secret before verifying the token
    
    * base64 url encoder
    
    * fix compile error
    
    * base64 url decode the key, (since it's encoded)
    
    * Cleanup, add comments

--- src/main/java/com/ibm/microservices/refapp/zuul/ZuulProxyApplication.java
+++ src/main/java/com/ibm/microservices/refapp/zuul/ZuulProxyApplication.java
@@ -1,12 +1,12 @@
 package com.ibm.microservices.refapp.zuul;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationContext;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.ibm.microservices.refapp.zuul.filters.pre.JWTValidationPreFilter;
 
 //import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
@@ -18,6 +18,12 @@ public class ZuulProxyApplication {
     public static void main(String[] args) {
 			SpringApplication.run(ZuulProxyApplication.class, args);
 			System.out.println("Running "+ZuulProxyApplication.class+" via Spring Boot!");
+    }
+    
+    
+    @Bean
+    public JWTValidationPreFilter authenticationFilter() {
+    	return new JWTValidationPreFilter();
     }
 
 }
