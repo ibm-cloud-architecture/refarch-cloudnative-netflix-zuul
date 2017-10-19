9420aa837b7c4b641a951e39a26bfbf247857c99
Jesus Almaraz <almarazj@ie.ibm.com>
2017-10-19 17:13:14 +0200

    merging changes from the RESILIENCY branch

--- src/test/java/com/ibm/microservices/refapp/zuul/ZuulProxyApplicationTests.java
+++ src/test/java/com/ibm/microservices/refapp/zuul/ZuulProxyApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//The commented line below come from the RESILIENCY branch.
+//@SpringBootTest( properties = { "spring.cloud.discovery.enabled:false" } )
 public class ZuulProxyApplicationTests {
 
 	@Test
